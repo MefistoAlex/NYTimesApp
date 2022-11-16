331583be9d9fbecd387a9f7a68ab755a4086c9b7
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-11-16 18:36:51 +0100

    deleted auto gen code

--- NYTimesApp/Views/ArticleTableViewCell/ArticleTableViewCell.swift
+++ NYTimesApp/Views/ArticleTableViewCell/ArticleTableViewCell.swift
@@ -16,17 +16,6 @@ final class ArticleTableViewCell: UITableViewCell {
     @IBOutlet private var titleLabel:
         UILabel!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
     func setArticle(_ article: Article) {
         titleLabel.text = article.title
         descriptionLabel.text = article.description
