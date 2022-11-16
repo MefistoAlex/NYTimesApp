12bdf2dce197b8d7835fb3f2f486d3e540225a64
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-11-16 22:39:01 +0200

    Create MostSharedViewController.swift

---
+++ NYTimesApp/Views/Controllers/MostSharedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MostSharedViewController.swift
+//  NYTimesApp
+//
+//  Created by Alexandr Mefisto on 16.11.2022.
+//
+import RxCocoa
+import RxSwift
+import UIKit
+
+final class MostSharedViewController: UIViewController {
+    private let disposeBag = DisposeBag()
+    private let newsViewModel = NewsViewModel.shared
+
+    @IBOutlet var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.register(UINib(nibName: String(describing: ArticleTableViewCell.self), bundle: nil), forCellReuseIdentifier: String(describing: ArticleTableViewCell.self))
+
+        newsViewModel.getNewsByCategory(.shared)
+
+        newsViewModel.mostShared.bind(to:
+            tableView.rx.items(
+                cellIdentifier: String(describing: ArticleTableViewCell.self),
+                cellType: ArticleTableViewCell.self)) { _, article, cell in
+            cell.setArticle(article)
+        }.disposed(by: disposeBag)
+    }
+}
