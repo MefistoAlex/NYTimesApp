f568c9f55a1d36639692adaa1e21d881c078e834
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-11-16 15:34:50 +0100

    Added API manager protocol and AlamofireAPIManager

---
+++ NYTimesApp/AlamofireAPIManager.swift
@@ -0,0 +1,31 @@
+//
+//  AlamofireAPIManager.swift
+//  NYTimesApp
+//
+//  Created by Alexandr Mefisto on 16.11.2022.
+//
+
+import Alamofire
+import Foundation
+final class AlamofireAPIManager: APIManager {
+    func request<T>(urlString: String,
+                    method: HttpMethod,
+                    dataType: T.Type,
+                    headers: [String: String]?,
+                    parameters: Parameters?,
+                    completion: @escaping (T?, Error?) -> Void) where T: Decodable {
+        var httpHeaders: HTTPHeaders?
+        if let headers = headers {
+            httpHeaders = HTTPHeaders(headers)
+        }
+
+        AF.request(urlString, method: HTTPMethod(rawValue: method.rawValue),
+                   parameters: parameters,
+//                   encoder: <#T##ParameterEncoder#>,
+                   headers: httpHeaders)
+            .validate()
+            .responseDecodable(of: T.self) { response in
+                completion(response.value, response.error)
+            }
+    }
+}
