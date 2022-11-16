940ce7e9a7d8b72a57bb3a161ef8d285bdee2e94
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-11-16 16:55:43 +0100

    Added Constants file

---
+++ NYTimesApp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  NYTimesApp
+//
+//  Created by Alexandr Mefisto on 16.11.2022.
+//
+
+import Foundation
+struct Constants {
+    static let header = ["Accept": "application/json"]
+    static let parameters = ["api-key": "msExutYA11mjjy1Af7i78edFEn69ZXvi"]
+}
