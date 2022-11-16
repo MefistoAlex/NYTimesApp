06c23ffdc82f4f4b61d337e33621f86e631595c2
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-11-16 17:44:52 +0100

    create MostEmailedViewController

---
+++ NYTimesApp/Views/MostEmailedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MostEmailedViewController.swift
+//  NYTimesApp
+//
+//  Created by Alexandr Mefisto on 16.11.2022.
+//
+
+import UIKit
+
+class MostEmailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
