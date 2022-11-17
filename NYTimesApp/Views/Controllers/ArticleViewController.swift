578657de9b0392e15237a01bc6c247ef9b3c132a
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-11-17 17:31:03 +0200

    Update ArticleViewController.swift
    adding and removing articles by favourite button

--- NYTimesApp/Views/Controllers/ArticleViewController.swift
+++ NYTimesApp/Views/Controllers/ArticleViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Alexandr Mefisto on 17.11.2022.
 //
 
+import RxSwift
 import UIKit
 import WebKit
 
@@ -16,16 +17,39 @@ final class ArticleViewController: UIViewController {
 
     // MARK: - Properties
 
+    private let newsViewModel = NewsViewModel.shared
+    private let disposeBag = DisposeBag()
     private var article: Article?
+    private var isFavourite = false {
+        didSet {
+            if isFavourite {
+                favouriteButton.image = UIImage(systemName: "star.fill")
+            } else {
+                favouriteButton.image = UIImage(systemName: "star")
+            }
+        }
+    }
 
     // MARK: - Lyfecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         if let article {
             let url = URL(string: article.url)
             let urlRequest = URLRequest(url: url!)
             webWiew.load(urlRequest)
+
+            let articles = try? newsViewModel.favourites.value()
+            if let articles {
+                let set = Set(articles)
+                isFavourite = set.contains(article)
+            }
+
+            newsViewModel.favourites.subscribe { event in
+                let set = Set(event.element!)
+                self.isFavourite = set.contains(self.article!)
+            }.disposed(by: disposeBag)
         }
     }
 
@@ -36,6 +60,13 @@ final class ArticleViewController: UIViewController {
     }
 
     @IBAction private func favouriteButtonDidTap(_ sender: UIBarButtonItem) {
+        if let article {
+            if isFavourite {
+                newsViewModel.deleteArticleFromFavourites(article)
+            } else {
+                newsViewModel.addArticleToFavourites(article)
+            }
+        }
     }
 
     func setArticle(_ article: Article) {
